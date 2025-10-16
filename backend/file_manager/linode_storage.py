970e2dfc92a21fb8e51395799c6be3ba52997499
Johan Karlsson <johan@colliberty.com>
2025-10-17 01:07:37 +0200

    Simplify Linode storage backend - force public-read ACL

--- backend/file_manager/linode_storage.py
+++ backend/file_manager/linode_storage.py
@@ -1,43 +1,32 @@
 """
 Custom storage backend for Linode Object Storage.
 
-Uses requests library instead of boto3 to work around boto3/urllib3 incompatibility
-with Linode Object Storage flexible checksums.
+Configured for boto3 1.26.137 (before flexible checksums) with public-read ACL.
 """
 
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-from botocore.client import Config
 
 
 class LinodeObjectStorage(S3Boto3Storage):
     """
     Custom storage backend for Linode Object Storage.
     
-    Disables AWS SDK flexible checksums which Linode doesn't support.
+    - Works with boto3 1.26.137 (before flexible checksums that broke Linode compatibility)
+    - Uploads files with public-read ACL for direct access
+    - Disables signed URLs since files are public
     """
     
-    def __init__(self, **settings_dict):
-        # Force client config that disables checksums
-        settings_dict.setdefault('client_config', Config(
-            s3={
-                'use_accelerate_endpoint': False,
-                'addressing_style': 'path',
-            },
-            signature_version='s3v4',
-        ))
-        
-        # Disable checksums globally
-        import os
-        os.environ['AWS_DISABLE_S3_CHECKSUM'] = 'true'
-        
-        super().__init__(**settings_dict)
-        
-    def _create_client(self):
-        """Override client creation to disable checksums."""
-        import os
-        # Disable all checksum features before creating client
-        os.environ['AWS_DISABLE_S3_CHECKSUM'] = 'true'
-        
-        return super()._create_client()
-
+    # Force public-read ACL on all uploads
+    default_acl = 'public-read'
+    
+    # Don't use signed URLs - files are public
+    querystring_auth = False
+    
+    # File settings
+    file_overwrite = False
+    
+    # Object parameters for uploads
+    object_parameters = {
+        'CacheControl': 'max-age=86400',
+        'ACL': 'public-read',
+    }
